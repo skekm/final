e5304a6bf6b2aa11762f6e10ae1bc68bdfca2b39
lost <105382415+skekm@users.noreply.github.com>
2025-06-13 23:45:19 +0200

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import discord, asyncio, random, os
+
+token = os.getenv('TOKEN')
+guild_id = 1382434954213855352
+channel_id = 1382831049381511258
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        await asyncio.sleep(random.randint(30,120))
+        g, c = self.get_guild(guild_id), self.get_guild(guild_id).get_channel(channel_id)
+        cmds = await c.application_commands()
+        bump = next((cmd for cmd in cmds if cmd.name == "bump"), None)
+        print(bump)
+
+client = MyClient()
+client.run(token)
