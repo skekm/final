02dabbf16a95b38f1361f7ea0681d638ade2287f
lost <105382415+skekm@users.noreply.github.com>
2025-06-14 00:01:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ class MyClient(discord.Client):
         g, c = self.get_guild(guild_id), self.get_guild(guild_id).get_channel(channel_id)
         cmds = await c.application_commands()
         bump = next((cmd for cmd in cmds if cmd.name == "bump"), None)
-        print(bump)
         await bump.__call__(channel=c)
         await self.close()
 
