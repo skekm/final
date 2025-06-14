499bc66874029de5d68df4c2800a2386716fc3eb
lost <105382415+skekm@users.noreply.github.com>
2025-06-14 18:29:23 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ channel_id = 1382831049381511258
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        await asyncio.sleep(random.randint(1,10))
         g, c = self.get_guild(guild_id), self.get_guild(guild_id).get_channel(channel_id)
         cmds = await c.application_commands()
         bump = next((cmd for cmd in cmds if cmd.name == "bump"), None)
